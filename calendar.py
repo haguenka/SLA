9b28e6ed5a95e1d4d952381ecc4d68f82f177631
haguenka <76132710+haguenka@users.noreply.github.com>
2024-10-15 19:10:11 -0300

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -69,7 +69,7 @@ if st.session_state['vacancy_data']:
                 day_date = date(year, month, day)
                 doctors = ", ".join(calendar_dict[day_date]) if day_date in calendar_dict else ""
                 fig.add_trace(go.Scatter(x=[week_days[day_idx]], y=[week_idx], mode='markers+text', marker=dict(size=70, color='darkslateblue', line=dict(width=3, color='white')), text=[f"Day {day}
-Doctors: {doctors}"],
+Doctors: {doctors}"]
                     textposition='top center',
                     textfont=dict(color='white')
                 ))
