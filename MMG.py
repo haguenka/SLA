1248df0a20b05652c244eaa04309023d9d0d83fd
haguenka <76132710+haguenka@users.noreply.github.com>
2024-10-15 12:53:22 -0300

    Update MMG.py

--- MMG.py
+++ MMG.py
@@ -46,7 +46,7 @@ if file is not None:
         filtered_df = mamografia_df[mamografia_df['UNIDADE'] == unidade_selected]
 
         if date_selection:
-            if isinstance(date_selection, list) and len(date_selection) == 2:
+            if isinstance(date_selection, tuple) and len(date_selection) == 2:
                 start_date, end_date = date_selection
                 filtered_df = filtered_df[(filtered_df['DATA_HORA_PRESCRICAO'].dt.date >= start_date) & 
                                           (filtered_df['DATA_HORA_PRESCRICAO'].dt.date <= end_date)]
