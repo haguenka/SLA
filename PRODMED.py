d1877ad450f93547c8154d2cea972c410937ee7e
haguenka <76132710+haguenka@users.noreply.github.com>
2024-10-23 15:56:24 -0300

    Update PRODMED.py

--- PRODMED.py
+++ PRODMED.py
@@ -145,7 +145,7 @@ grid_options = gb.build()
 
 selected_rows = AgGrid(days_grouped, gridOptions=grid_options, enable_enterprise_modules=False, width='100%')['selected_rows']
 if len(selected_rows) > 0:
-    selected_index = selected_rows[0]['_selectedRowNodeInfo']['nodeIndex']
+    selected_index = selected_rows[0]['_selectedRowNodeInfo']['rowIndex']
     show_filtered_data(selected_index)
 
 # Plot events per hour for each day
