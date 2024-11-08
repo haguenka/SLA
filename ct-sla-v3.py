5879bf461d0a18b2acce99b9adef596713fe410f
haguenka <76132710+haguenka@users.noreply.github.com>
2024-11-08 08:49:54 -0300

    Update ct-sla-v3.py

--- ct-sla-v3.py
+++ ct-sla-v3.py
@@ -12,8 +12,14 @@ def load_logo(url):
     return Image.open(BytesIO(response.content))
 
 @st.cache_data
-def load_excel(uploaded_file):
-    return pd.read_excel(uploaded_file)
+def load_excel_from_github():
+    try:
+        url = 'https://raw.githubusercontent.com/haguenka/SLA/main/basesla4.xlsx'
+        response = requests.get(url)
+        response.raise_for_status()
+        return pd.read_excel(BytesIO(response.content))
+    except requests.exceptions.RequestException:
+        return None
 
 # Streamlit file uploader
 st.title("SLA Dashboard for CT Exams")
@@ -24,11 +30,11 @@ response = requests.get(url)
 logo = Image.open(BytesIO(response.content))
 st.sidebar.image(logo, use_column_width=True)
 
-uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx"])
+#uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx"])
 
 if uploaded_file is not None:
-    # Load the uploaded Excel file
-    df = load_excel(uploaded_file)
+    # Load Excel file from GitHub if available
+    df = load_excel_from_github()
 
     # Filter the data for 'CT' modality and 'Pronto Atendimento'
     filtered_df = df[(df['MODALIDADE'] == 'CT') & (df['TIPO_ATENDIMENTO'] == 'Pronto Atendimento')]
