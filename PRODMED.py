a2559f0b836757f8a1e23a05340ec1e4f5f146e8
haguenka <76132710+haguenka@users.noreply.github.com>
2024-11-22 08:38:56 -0300

    Update PRODMED.py

--- PRODMED.py
+++ PRODMED.py
@@ -32,7 +32,7 @@ st.sidebar.image(logo, use_column_width=True)
 st.title('Medical Production Report')
 
 # Load Excel and CSV files from GitHub
-xlsx_url = 'https://raw.githubusercontent.com/haguenka/SLA/main/basesla6.xlsx'
+xlsx_url = 'https://raw.githubusercontent.com/haguenka/SLA/main/basesla8M.xlsx'
 csv_url = 'https://raw.githubusercontent.com/haguenka/SLA/main/multipliers.csv'
 
 excel_df = load_excel_data(xlsx_url)
