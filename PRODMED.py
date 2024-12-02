cdbb10b04a866357cc65f74f639b99b97c06d74a
haguenka <76132710+haguenka@users.noreply.github.com>
2024-12-02 14:13:19 -0300

    Update PRODMED.py

--- PRODMED.py
+++ PRODMED.py
@@ -107,7 +107,7 @@ st.markdown(f"<h2 style='color:#10fa07;'>Total Points for All Modalities: {total
 
 # Get the days and periods each doctor has events
 days_df = filtered_df[['MEDICO_LAUDO_DEFINITIVO', 'STATUS_APROVADO']].dropna()
-days_df['DAY_OF_WEEK'] = days_df['DATA_LAUDO'].dt.strftime('%A').replace({'Monday': 'Segunda-feira', 'Tuesday': 'Terça-feira', 'Wednesday': 'Quarta-feira', 'Thursday': 'Quinta-feira', 'Friday': 'Sexta-feira', 'Saturday': 'Sábado', 'Sunday': 'Domingo'})
+days_df['DAY_OF_WEEK'] = days_df['STATUS_APROVADO'].dt.strftime('%A').replace({'Monday': 'Segunda-feira', 'Tuesday': 'Terça-feira', 'Wednesday': 'Quarta-feira', 'Thursday': 'Quinta-feira', 'Friday': 'Sexta-feira', 'Saturday': 'Sábado', 'Sunday': 'Domingo'})
 days_df['DATE'] = days_df['DATA_LAUDO'].dt.strftime('%Y-%m-%d')
 
 # Define time periods
