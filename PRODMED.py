29a7ed8612cc5f258b607d36bda2e30129b51ea3
haguenka <76132710+haguenka@users.noreply.github.com>
2024-12-13 15:00:00 -0300

    Update PRODMED.py

--- PRODMED.py
+++ PRODMED.py
@@ -5,6 +5,7 @@ import requests
 from io import BytesIO
 import matplotlib.pyplot as plt
 from datetime import datetime
+from fpdf import FPDF
 
 # Caching for performance improvement
 @st.cache_data
