d0dff414094c2b01bfd26ecc2aec398f091ecafd
haguenka <76132710+haguenka@users.noreply.github.com>
2025-02-12 13:51:00 -0300

    Update prescritor.py

--- prescritor.py
+++ prescritor.py
@@ -1,11 +1,70 @@
 import streamlit as st
 import pandas as pd
+import requests
+from PIL import Image
+from io import BytesIO
 
-# Carregar os dados
+# Função para carregar o logo via URL com cache
+@st.cache_data(show_spinner=False)
+def load_logo(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return Image.open(BytesIO(response.content))
+    else:
+        return None
+
+# Função para carregar o arquivo Excel via URL com cache
+@st.cache_data(show_spinner=False)
+def load_excel_data(xlsx_url):
+    response = requests.get(xlsx_url)
+    if response.status_code == 200:
+        return pd.read_excel(BytesIO(response.content))
+    else:
+        st.error("Não foi possível carregar o arquivo Excel da URL fornecida.")
+        return None
+
+# URL do logo
+url_logo = 'https://raw.githubusercontent.com/haguenka/SLA/main/sj.png'
+logo = load_logo(url_logo)
+
+# -------------------------------
+# CONFIGURAÇÃO DE CSS (DARK MODE)
+# -------------------------------
+st.markdown("""
+    <style>
+    /* Estilo para o corpo (modo escuro) */
+    body {
+        background-color: #121212;
+        color: #ffffff;
+    }
+    /* Estilo para a sidebar */
+    .css-1d391kg, .css-1d391kg div {
+        background-color: #1e1e1e;
+    }
+    /* Ajuste de fonte e espaçamento para um visual clean/fancy */
+    .reportview-container .main .block-container{
+        padding-top: 2rem;
+        padding-right: 2rem;
+        padding-left: 2rem;
+        padding-bottom: 2rem;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+# -------------------------------
+# TÍTULO E LOGO NA SIDEBAR
+# -------------------------------
+st.title("Rastreador de Cálculo Renal CSSJ")
+if logo is not None:
+    st.sidebar.image(logo, use_container_width=True)
+st.sidebar.header("Selecione os Arquivos")
+
+# Função para carregar os dados do repositório Git
 def load_data():
-    file_path = "teste.xlsx"
-    df = pd.read_excel(file_path, sheet_name="Sheet1")
-    df["DATA_HORA_PRESCRICAO"] = pd.to_datetime(df["DATA_HORA_PRESCRICAO"], errors='coerce')
+    xlsx_url = 'https://raw.githubusercontent.com/haguenka/SLA/main/baseslaM.xlsx'
+    df = load_excel_data(xlsx_url)
+    if df is not None:
+        df["DATA_HORA_PRESCRICAO"] = pd.to_datetime(df["DATA_HORA_PRESCRICAO"], errors='coerce')
     return df
 
 df = load_data()
