d408073b97381767601aeb115ef1c93c4c406e81
haguenka <76132710+haguenka@users.noreply.github.com>
2025-02-12 15:06:11 -0300

    Update prescritor.py

--- prescritor.py
+++ prescritor.py
@@ -141,3 +141,10 @@ with tab2:
     st.header("Top 10 Médicos Prescritores")
     top_medicos = df_filtrado["MEDICO_SOLICITANTE"].value_counts().head(10)
     st.bar_chart(top_medicos)
+
+    st.header("Top 10 Médicos Prescritores de RM")
+        # Filtrar registros cuja coluna MODALIDADE contenha "RM"
+        df_rm = df_filtrado[df_filtrado["MODALIDADE"].str.contains("RM", case=False, na=False)]
+        # Calcular o top 10 de médicos prescritores para exames de RM
+        top_medicos_rm = df_rm["MEDICO_SOLICITANTE"].value_counts().head(10)
+        st.bar_chart(top_medicos_rm)
